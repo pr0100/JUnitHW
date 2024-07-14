4ab375d722cc80fd18c09a50cf26906a288d9770
pr0100 <ssstrelkov2000@gmail.com>
2024-07-15 02:22:41 +0500

    commit project

---
+++ src/test/java/homework/SimpleTest.java
@@ -0,0 +1,14 @@
+package homework;
+
+import org.junit.jupiter.api.*;
+
+public class TestNew {
+
+    @Test
+    @DisplayName("Первый тест")
+     void SimpleTest() {
+        System.out.println("Hello World!");
+    }
+
+
+}
---
+++ src/test/java/homework/data/Language.java
@@ -0,0 +1,2 @@
+package homework.data;public enum Language {
+}
